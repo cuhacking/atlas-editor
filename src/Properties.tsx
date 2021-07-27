78c0e126b9491bc391ef6fac2833a07aaa777d85
deenhaque <fardeen2000@hotmail.com>
2021-07-27 14:24:31 -0400

    User can now click on a feature and view its properties on the right hand side

--- src/Properties.tsx
+++ src/Properties.tsx
@@ -1,65 +1,58 @@
 import React, { Fragment } from 'react';
-import { FeatureCollection } from 'geojson';
-
+import { Feature } from 'geojson';
 interface PropertyProps {
-  info: FeatureCollection[] | null;
+  feature: Feature;
 }
 
-const Properties: React.FC<PropertyProps> = (props: PropertyProps) => {
-  console.log('props', props);
-  const { info } = props;
-  console.log('info', info);
-
-  return (
-    <div>
-      {info ? (
-        info.map((inf, i) =>
-          inf ? (
-            <Fragment key={`${i}-fr-inf`}>
-              <ul key={`${i}-ul-inf`}>
-                Properties:{' '}
-                {inf.features.map((feature, j) => {
-                  const { properties } = feature;
-                  // console.log('properties', properties);
-                  return properties ? (
-                    <Fragment key={`${j}-pr-fr-inf`}>
-                      <br />
-                      <li key={`${properties.fid}-map-properties`}>
-                        {properties.building}
-                        {properties.roomId}
-                      </li>
-                      <li> Floor: {properties.floor}</li>
-                      <li>
-                        {' '}
-                        Room: {properties.roomName} ({properties.roomType})
-                      </li>
-                      <li>
-                        Search Tags:
-                        <ul>
-                          {properties.searchTagProps.map(
-                            (searchTag: string, k: number) => (
-                              <li key={`${k}-search=tag`}>{searchTag}</li>
-                            )
-                          )}
-                        </ul>
-                      </li>
-                      <br />
-                    </Fragment>
-                  ) : (
-                    'N/A'
-                  );
-                })}
-              </ul>
-            </Fragment>
-          ) : (
-            <p key={`${i}-err-inf`}>Something may have gone wrong</p>
-          )
-        )
-      ) : (
-        <p>Open a file!</p>
+const Property = ({ feature }: PropertyProps) => {
+  const { properties } = feature;
+  return properties ? (
+    <Fragment key={`${properties.building}-${properties.roomId}-property`}>
+      <br />
+      <li>
+        {properties.building}
+        {properties.roomId}
+      </li>
+      <li> Floor: {properties.floor}</li>
+      <li>
+        {' '}
+        Room: {properties.roomName} ({properties.roomType})
+      </li>
+      {Array.isArray(properties.searchTagProps) && (
+        <li>
+          Search Tags:
+          <ul>
+            {properties.searchTagProps.map((searchTag: string, k: number) => (
+              <li key={`${k}-property-search-tag`}>{searchTag}</li>
+            ))}
+          </ul>
+        </li>
       )}
-    </div>
+      <br />
+    </Fragment>
+  ) : (
+    <p>Something may have gone wrong</p>
   );
 };
+interface PropertiesProps {
+  features: Feature[] | null;
+}
+
+const Properties: React.FC<PropertiesProps> = ({
+  features
+}: PropertiesProps) => (
+  <>
+    {console.log('User just clicked on the following feature(s):', features)}
+    {features ? (
+      features.map((feature, i) => (
+        <ul key={`${i}-ul-feature`}>
+          Properties: <Property feature={feature} />
+        </ul>
+      ))
+    ) : (
+      <p>Click on something!</p>
+    )}
+  </>
+);
 
 export default Properties;
